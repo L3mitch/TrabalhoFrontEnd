0ba60cda078b4cbb40b900d74b82a45837ddeb2b
Vitor Wessler <vitorwessler@gmail.com>
2024-06-24 16:17:12 -0300

    NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,20 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import './NavBar.css';
 
 export default function Navbar() {
     const [usuarioAtual, setUsuarioAtual] = useState(null);
     const navigate = useNavigate();
 
-
     useEffect(() => {
         const usuarioString = localStorage.getItem('user');
         if (usuarioString) {
             const user = JSON.parse(usuarioString);
-            setUsuarioAtual(user.nome)
-        }
-        else {
+            setUsuarioAtual(user.nome);
+        } else {
             navigate('/');
         }
-    }, []);
+    }, [navigate]);
 
     const handleLogout = () => {
         localStorage.removeItem('user');
@@ -27,6 +25,10 @@ export default function Navbar() {
         <div className="navbar">
             <div className="logo-container">
                 <h3>Sistema de Contabilidade de Produtos</h3>
+            </div>
+            <div>
+                <Link to={'/users'}>Manutenção de Usuários</Link>
+                <Link to={'/products'}>Manutenção de Produtos</Link>
             </div>
             <div className="user-info">
                 <span>Bem vindo, {usuarioAtual}</span>
